1387400ef2a27b9f283cc4bf091dce04d76ab077
disvpn <168737287+disvpn@users.noreply.github.com>
2024-06-03 11:15:40 +0330

    _worker.js

--- _worker.js
+++ _worker.js
@@ -1,7 +1,7 @@
 // @armanibash
 import { connect } from 'cloudflare:sockets';
 
-let userID = '652da35d-b59d-4ab0-af69-cb90066dcc75';
+let userID = '0eadb777-2fe3-4b2c-9e79-40764b2dc6ae';
 
 const proxyIPs = ["[2001:67c:2b0:db32:0:1:681a:404]"]; // ['cdn-all.xn--b6gac.eu.org', 'cdn.xn--b6gac.eu.org', 'cdn-b100.xn--b6gac.eu.org', 'edgetunnel.anycast.eu.org', 'cdn.anycast.eu.org'];
 let proxyIP = proxyIPs[Math.floor(Math.random() * proxyIPs.length)];
